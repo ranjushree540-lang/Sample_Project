b4a99999edc3e1270ceb670a9242ebb91ea69ce8
ranjushree540-lang <ranjushree540@gmail.com>
2025-09-15 15:51:06 -0600

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        // Create a scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Ask for user input
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        
+        // Perform the addition
+        double sum = num1 + num2;
+        
+        // Output the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        
+        // Close the scanner
+        scanner.close();
+    }
+}
