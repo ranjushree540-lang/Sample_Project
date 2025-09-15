2b2908c2155a2418c4f8176f07d8248deb202994
ranjushree540-lang <ranjushree540@gmail.com>
2025-09-15 16:32:32 -0600

    Create file3.java

---
+++ src/file3.java
@@ -0,0 +1,13 @@
+public class Addition {
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 3;
+        
+        
+        int sum = num1 + num2;
+        
+      
+        System.out.println("The sum is: " + sum);
+    }
+}
