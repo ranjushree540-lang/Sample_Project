f1337da3dff65989e973e05ae544a398be3d506d
ranjushree540-lang <ranjushree540@gmail.com>
2025-09-15 16:41:31 -0600

    Create file6.py

---
+++ src/file6.py
@@ -0,0 +1,14 @@
+# Initial list
+my_list = [1, 2, 3]
+
+# Adding an item using append()
+my_list.append(4)
+print("List after appending 4:", my_list)
+
+# Adding multiple items using extend()
+my_list.extend([5, 6, 7])
+print("List after extending with [5, 6, 7]:", my_list)
+
+# Adding an item at a specific index using insert()
+my_list.insert(1, 10)  # Adds 10 at index 1
+print("List after inserting 10 at index 1:", my_list)
