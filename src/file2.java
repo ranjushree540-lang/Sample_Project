2a4bb0704770f89d9a5941e980e4909757db4f57
ranjushree540-lang <ranjushree540@gmail.com>
2025-09-15 16:31:31 -0600

    Create file2.java

---
+++ src/file2.java
@@ -0,0 +1,13 @@
+public class SimpleAddition {
+    public static void main(String[] args) {
+        // Declare two variables for storing numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Add the two numbers
+        int sum = num1 + num2;
+
+        // Print the result to the console
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
